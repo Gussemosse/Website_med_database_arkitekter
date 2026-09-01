76ce537bb105e9fd680e37f88f31bc48c4f9c858
Gussemosse <gustavgandrup@gmail.com>
2026-09-01 12:52:49 +0200

    Opsætning af Flask

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 from flask import Flask, request, render_template
 import sqlite3
+
+app = Flask(__name__)
+# DB
+DB_ARCHITECTS = "./db/arcitects.db"
+
+# Routes
+@app.route("/")
+def index():
+    return render_template("index.html", title="Home Page")
+
+# Start Flask server
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
